32983a08746f62e00ba3406aecd8bf91eeb33c51
dga <dga@danielgarnold.com>
2019-10-08 11:33:32 -0700

    Add exception handling and refactor menu output

--- stegano.py
+++ stegano.py
@@ -2,22 +2,32 @@ import encrypt as e
 import decrypt as d
 
 def main():
+    heading = "#################### Encryption Tool ###################"
+    menu_opts = ["Encrypt file", "Decrypt File", "Quit"]
+
     while True:
-        print("#################### Encryption Tool ###################\n"
-            "1) Encrypt file\n"
-            "2) Decrypt file\n"
-            "3) Quit")
-        user_input = int(input("Execute: "))
-        if user_input == 1:
-            e.encryption_menu()
-            break
-        elif user_input == 2:
-            d.decryption_menu()
-            break
-        elif user_input == 3:
-            break
-        else:
-            print('Incorrect input')
+        print(heading)
+        for i, option in enumerate(menu_opts):
+            print(f"{i + 1}. {option}")
+
+        try:
+            user_input = int(input(f"{'-' * len(heading)}\nExecute: "))
+
+            if user_input not in range(1, 4):
+                raise ValueError
+            else:
+                break
+
+        except ValueError:
+            print("\nError: Invalid menu option\n")
+
+    if user_input == 1:
+        e.encryption_menu()
+    elif user_input == 2:
+        d.decryption_menu()
+    elif user_input == 3:
+        print('Exiting.')
+           
             
 if __name__ == '__main__' : 
     main()
